faefc7a82b53f357e532c039b49e674e0f59d188
Reegan McAllister <mcallisterreegan@gmail.com>
2016-04-27 14:07:54 -0600

    Added files via upload

---
+++ XYZPlot.m
@@ -0,0 +1,99 @@
+classdef XYZPlot<handle
+    %XYZPlot Creates an plot of XYZ coordinates wrt time
+    %   Detailed explanation goes here
+    
+    properties
+        plotsize; %number of data points displayed on plot
+        datasize; %max data stored in data array
+        %arrays for holding data
+        x = [];
+        y = [];
+        z = [];
+        t = [];
+        
+        %keeping track of amount of data points
+        index;
+        %plot handles
+        xplot;
+        yplot;
+        zplot;
+    end
+    
+    methods
+        function obj=XYZPlot(plotdisplaysize,datastoragesize)
+            %class constructor
+            if nargin == 0
+                obj.datasize = 1000;
+                obj.plotsize = 100;
+            end
+            if nargin == 1
+                obj.datasize = 1000;
+                obj.plotsize = plotdisplaysize;
+            end
+            if nargin == 2
+                obj.plotsize = plotdisplaysize;
+                obj.datasize = datastoragesize;
+            end
+            
+            
+            obj.index = 0;
+            
+            %%preallocated ram for preformance
+            %data
+            obj.x = zeros(1,obj.datasize);
+            obj.y = zeros(1,obj.datasize);
+            obj.z = zeros(1,obj.datasize);
+            obj.t = zeros(1,obj.datasize);
+            
+            %%create subplots
+            %x
+            subplot(3,1,1);
+            obj.xplot = plot(obj.t,obj.x);
+            ylabel('X');
+            %y
+            subplot(3,1,2);
+            obj.yplot = plot(obj.t,obj.y);
+            ylabel('Y');
+            %z
+            subplot(3,1,3);
+            obj.zplot = plot(obj.t,obj.z);
+            xlabel('TIME');
+            ylabel('Z');
+        end
+        
+        function obj=addpoint(obj,xval,yval,zval,tval)
+            %adds data to plot
+            obj.index = obj.index + 1;
+            if(nargin > 0)
+                %update data
+                obj.x(obj.index) = xval;
+                obj.y(obj.index) = yval;
+                obj.z(obj.index) = zval;
+                obj.t(obj.index) = tval;
+                
+                %update plots
+                if obj.index <= obj.plotsize
+                    set(obj.xplot,'XData',obj.t,'YData',obj.x);
+                    set(obj.yplot,'XData',obj.t,'YData',obj.y);
+                    set(obj.zplot,'XData',obj.t,'YData',obj.z);
+                else
+                    set(obj.xplot,'XData',obj.t((obj.index-obj.plotsize):obj.index),'YData',obj.x((obj.index-obj.plotsize):obj.index));
+                    set(obj.yplot,'XData',obj.t((obj.index-obj.plotsize):obj.index),'YData',obj.y((obj.index-obj.plotsize):obj.index));
+                    set(obj.zplot,'XData',obj.t((obj.index-obj.plotsize):obj.index),'YData',obj.z((obj.index-obj.plotsize):obj.index));
+                end
+                
+                %limit ram usage
+                if obj.index  == obj.datasize
+                    %reset data
+                    obj.index  = 0;
+                    obj.x = zeros(1,obj.datasize);
+                    obj.y = zeros(1,obj.datasize);
+                    obj.z = zeros(1,obj.datasize);
+                    obj.t = zeros(1,obj.datasize);
+                end
+            end
+        end
+        
+    end
+end
+
---
+++ XYZPlot_example.m
@@ -0,0 +1,16 @@
+%this script creates a running x y z plot
+
+clear all
+
+plotsize = 360;
+
+%define refresh delay rate
+speed = 0.01;
+
+%make plot
+myplot = XYZPlot(plotsize);
+
+for i = [1:1000]
+    myplot.addpoint(sind(i),cosd(i),tand(i),i*speed);
+    pause(speed); %find a better method of timing than this
+end
